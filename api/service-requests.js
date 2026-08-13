de719d29a2824cfab494bbce69ad67e9a446b031
Natalia <nns0808@gmail.com>
2026-08-13 13:54:54 -0700

    Improve service request form and API

--- api/service-requests.js
+++ api/service-requests.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.get("/api/service-requests", (req, res) => {
   res.status(200).json({
     success: true,
