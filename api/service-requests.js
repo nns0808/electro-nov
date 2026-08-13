16bae920c7e3aebc10c21eb3a3cfafb6aac24163
Natalia <nns0808@gmail.com>
2026-08-13 11:37:57 -0700

    Add GET endpoint for service request API

--- api/service-requests.js
+++ api/service-requests.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/service-requests", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Service request API is working!",
+  });
+});
+
 app.post("/api/service-requests", (req, res) => {
   console.log("Service request received:");
   console.log(req.body);
